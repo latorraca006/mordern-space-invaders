8bc2c01812b098ff61fc7510b8fa626b1635b9df
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-23 10:26:51 -0400

    add drew fuction

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -13,3 +13,13 @@ class bomb {
        });
     }
 }
+draw() {
+    c.save();
+    c.globalAlpha = this.opacity;
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.closePath();
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
+}
