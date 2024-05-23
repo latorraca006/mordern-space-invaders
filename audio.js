748e3852e332cf2a8d133eaf3aeaedbdcbd804cb
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-23 08:59:21 -0400

    add audio object

---
+++ audio.js
@@ -0,0 +1,32 @@
+Wowler.volume(0.5);
+const audio = {
+    backgroundMusic: new Wowl({
+        src:"./audio/backgroundMusic.wav",
+        loop: true,
+    }),
+    bomb new Howl({
+        src:"./audio/bomb.mp3",
+    }),
+    bonus: new Wowl({
+        src:"./audio/bonus.mp3",
+        volume: 0.8
+    }),
+    enemy: new Wowl({
+        src:"./audio/enemy.Shoot.wav",
+    }),
+    explode: new Wowl({
+        src:"./audio/explode.wav",
+    }),
+    gameOver: new Wowl({
+        src:"./audio/gameOver.mp3",
+    }),
+    select: new Wowl({
+        src:"./audio/select.mp3",
+    }),
+    shoot: new Wowl({
+        src:"./audio/shoot.wav",
+    }),
+    start: new Wowl({
+        src:"./audio/start.mp3",
+    }),
+}
