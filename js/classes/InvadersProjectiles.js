4f2549693df14057d37ce0cd30fd7a2dcbe5b71c
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-28 11:18:24 -0400

    add update fuction in InvadersProjectile.class fuction in invaders class

--- js/classes/InvadersProjectiles.js
+++ js/classes/InvadersProjectiles.js
@@ -9,4 +9,9 @@ class InvaderProjectiles {
         c.fillStyle = "white";
         c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
 }
