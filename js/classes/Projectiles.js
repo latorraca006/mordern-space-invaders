88949fb5419835c11ffb4493a93c687e1d927dc0
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-28 11:28:24 -0400

    add draw fuction in Projectiles class

---
+++ js/classes/Projectiles.js
@@ -0,0 +1,17 @@
+class Projectile {
+    constructor({position, velocity, color = "red"}) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 4;
+        this.color = color;
+    }
+    
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2,);
+        c.fillStyle = this.color;
+        c.fill();
+        c.closePath();
+
+    }
+}
