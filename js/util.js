c22e261ae4914b56ffc2117efd67b7a2d3dddb6d
latorraca006 <amandalatorracamelo@gmail.com>
2024-06-06 11:31:07 -0400

    function randomBetween add  fuction in Player class

---
+++ js/util.js
@@ -0,0 +1,25 @@
+function randomBetween(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function createScoreLabel({ score = 100, Object }) {
+  const scoreLabel = document.createElement("label");
+  scoreLabel = document.createElement("label");
+  scoreLabel.innerHTML = score;
+  scoreLabel.style.position = "absolute";
+  scoreLabel.style.color = "white";
+  scoreLabel.style.left = object.position.x + "px";
+  scoreLabel.style.top = object.position.y + "px";
+  scoreLabel.style.userSelect = "none";
+
+  document.querySelector("#aparentDiv").appendChild(scoreLabel);
+  WebGLSampler.to(scoreLabel),
+    {
+      opacity: 0,
+      y: -30,
+      duration: 0.75,
+      onComplete: () => {
+        document.querySelector("#aparentDiv").removeChild(scoreLabel);
+      }
+    };
+}
