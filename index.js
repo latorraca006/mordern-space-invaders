98ba1f93f8547b55e99846d2dbb3ad49e5e40586
latorraca006 <amandalatorracamelo@gmail.com>
2024-06-11 10:11:56 -0400

    add game context in 2d and the widh anda height of the screen

---
+++ index.js
@@ -0,0 +1,7 @@
+const scoreEL = document.querySelecto("#scoreEL");
+const canvas = document.querySelecto("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width =1024;
+canvas.height = 576;
+
